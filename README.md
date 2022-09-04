5bac98f28d8a8758a9aa298e72bb90482daffd6b
no.later <no.later.cn@gmail.com>
2022-09-04 23:16:27 +0800

    feat: contracts build

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+# init project
+
+```javascript
+mkdir hardhat-mint-nft
+cd hardhat-mint-nft
+yarn add hardhat -D
+npx hardhat
+yarn add @openzeppelin/contracts
+```
+
 # Sample Hardhat Project
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
@@ -11,3 +21,5 @@ GAS_REPORT=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+xw
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import dotenv from 'dotenv';
+import '@nomicfoundation/hardhat-toolbox';
+
+dotenv.config();
+console.log(process.env);
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    rinkeby: {
+      url: process.env.REACT_APP_ROPSTEN_ROC_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY,
+  },
 };
 
 export default config;
